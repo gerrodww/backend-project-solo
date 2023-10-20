7786a7d24cfeef80aaf5cabe1cd141652a6fd4f1
Gerrod White <gerrodww@gmail.com>
2023-10-19 21:18:02 -0400

    Bookings seed adj

--- backend/db/seeders/20231020005103-bookings-demo.js
+++ backend/db/seeders/20231020005103-bookings-demo.js
@@ -11,56 +11,56 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await Booking.bulkCreate([
       {
-        spotId: 1,
-        userId: 9,
+        spotId: 9,
+        userId: 3,
         startDate: new Date('2023-11-01'),
         endDate: new Date('2023-11-10')
       },
       {
-        spotId: 2,
-        userId: 8,
+        spotId: 8,
+        userId: 4,
         startDate: new Date('2023-12-20'),
         endDate: new Date('2024-01-05')
       },
       {
-        spotId: 3,
-        userId: 7,
+        spotId: 7,
+        userId: 5,
         startDate: new Date('2024-01-01'),
         endDate: new Date('2024-12-31')
       },
       {
-        spotId: 4,
-        userId: 6,
+        spotId: 6,
+        userId: 1,
         startDate: new Date('2023-11-15'),
         endDate: new Date('2023-12-15')
       },
       {
-        spotId: 5,
-        userId: 4,
+        spotId: 4,
+        userId: 2,
         startDate: new Date('2023-12-01'),
         endDate: new Date('2023-12-31')
       },
       {
-        spotId: 6,
-        userId: 5,
+        spotId: 5,
+        userId: 7,
         startDate: new Date('2024-02-01'),
         endDate: new Date('2024-03-05')
       },
       {
-        spotId: 7,
-        userId: 3,
+        spotId: 3,
+        userId: 6,
         startDate: new Date('2024-01-05'),
         endDate: new Date('2024-05-01')
       },
       {
-        spotId: 8,
-        userId: 2,
+        spotId: 2,
+        userId: 8,
         startDate: new Date('2024-01-01'),
         endDate: new Date('2024-05-06')
       },
       {
-        spotId: 9,
-        userId: 1,
+        spotId: 1,
+        userId: 9,
         startDate: new Date('2024-09-18'),
         endDate: new Date('2026-09-18')
       },
