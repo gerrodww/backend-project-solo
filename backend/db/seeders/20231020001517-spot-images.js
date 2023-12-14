42e0a9ee0d5ca753c35b7da692d4dd6554a83a9a
Gerrod White <gerrodww@gmail.com>
2023-12-13 21:04:21 -0500

    didnt break

--- backend/db/seeders/20231020001517-spot-images.js
+++ backend/db/seeders/20231020001517-spot-images.js
@@ -12,28 +12,28 @@ module.exports = {
     await SpotImage.bulkCreate([
       {
         spotId: 1,
-        url: "wallabywayimage.url",
+        url: "https://assets.architecturaldesigns.com/plan_assets/325002687/thumb/86086BW_Render_1561997573.jpg",
         preview: true,
       },
       {
         spotId: 2,
-        url: "starfallimage.url",
+        url: "https://cdn.getyourguide.com/img/tour/617996e3f251c.jpeg/145.jpg",
         preview: true,
       },
       {
         spotId: 3,
-        url: "shireimage.url",
+        url: "https://images.discerningassets.com/image/upload/v1628262039/Hobbit_House_3_np4qab.jpg",
         preview: true,
       },
       {
         spotId: 4,
-        url: "dungeonimage.url",
+        url: "https://t4.ftcdn.net/jpg/03/47/29/29/360_F_347292952_H95fWzKSglfB6qCllbD2CKvBcNyrRVvZ.jpg",
         preview: true,
       },
       {
         spotId: 5,
-        url: "caveimage.url",
-        preview: false,
+        url: "https://img.freepik.com/premium-photo/camping-cave_445983-2636.jpg",
+        preview: true,
       },
     ], { validate: true});
   },
